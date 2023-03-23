d8d4bcc87baa8dab31b042d1f05e95a5bad888f0
Mari3471 <127406567+Mari3471@users.noreply.github.com>
2023-03-23 12:54:24 +0100

    1 binaries uploaded

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.listen(3000, () => {
   console.log('Hello from SPSE!');
 });
-app.use(static);
+app.use('/static', express.static('static'));
 app.get('/', (req, res) => {
   res.write('<!DOCTYPE html>');
   res.write('<html lang="cs">');
@@ -15,7 +15,9 @@ app.get('/', (req, res) => {
   res.write('<body>');
   res.write('<h1>SQL je na sebevrazdu</h1>');
   res.write('<a href="/stats">STATISTICS</a>');
+  res.write('<br>');
   res.write('<a href="/static/Kuchari.html">Studenti v okne</a>');
+  res.write('<br>');
   res.write('</body>');
   res.write('</html>');
   res.end();
